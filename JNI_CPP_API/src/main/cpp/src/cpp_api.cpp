6ca532ffcccc9db1c42828188e019e11d1aaa314
mgood7123 <smallville7123@gmail.com>
2021-04-01 14:20:09 +1000

    work on resolvePath

--- JNI_CPP_API/src/main/cpp/src/cpp_api.cpp
+++ JNI_CPP_API/src/main/cpp/src/cpp_api.cpp
@@ -25,3 +25,25 @@ Java_smallville7123_AndroidDAW_SDK_jni_1cpp_1api_JNI_1CPP_1API_dirname(JNIEnv *e
     JniHelpers::Strings::deleteJniStringUTF(&str);
     return JniHelpers::References::toGlobal(env, obj);
 }
+
+std::string resolvePathInternal(const char * cwd, const char * path) {
+    return "";
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_smallville7123_AndroidDAW_SDK_jni_1cpp_1api_JNI_1CPP_1API_00024Extras_resolvePath(JNIEnv *env,
+                               jclass clazz,
+                               jstring cwd,
+                               jstring path) {
+    const char * cwd_ = JniHelpers::Strings::newJniStringUTF(env, cwd);
+    const char * path_ = JniHelpers::Strings::newJniStringUTF(env, path);
+
+    std::string result = resolvePathInternal(cwd_, path_);
+
+    JniHelpers::Strings::deleteJniStringUTF(&cwd_);
+    JniHelpers::Strings::deleteJniStringUTF(&path_);
+
+    jstring obj = JniHelpers::Strings::newString(env, result);
+    return JniHelpers::References::toGlobal(env, obj);
+}
--- JNI_CPP_API/src/main/java/smallville7123/AndroidDAW/SDK/jni_cpp_api/JNI_CPP_API.java
+++ JNI_CPP_API/src/main/java/smallville7123/AndroidDAW/SDK/jni_cpp_api/JNI_CPP_API.java
@@ -3,4 +3,7 @@ package smallville7123.AndroidDAW.SDK.jni_cpp_api;
 public class JNI_CPP_API {
     public static native String basename(String path);
     public static native String dirname(String path);
+    public static class Extras {
+        public static native String resolvePath(String cwd, String path);
+    }
 }
