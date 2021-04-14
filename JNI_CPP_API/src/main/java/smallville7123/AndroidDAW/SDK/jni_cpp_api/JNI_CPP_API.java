af35c4f5529d165d6710cd6788c20d017ecd5153
mgood7123 <smallville7123@gmail.com>
2021-04-14 20:43:11 +1000

    JNI_CPP_API now auto-load's its libraries

--- JNI_CPP_API/src/main/java/smallville7123/AndroidDAW/SDK/jni_cpp_api/JNI_CPP_API.java
+++ JNI_CPP_API/src/main/java/smallville7123/AndroidDAW/SDK/jni_cpp_api/JNI_CPP_API.java
@@ -1,10 +1,34 @@
 package smallville7123.AndroidDAW.SDK.jni_cpp_api;
 
 public class JNI_CPP_API {
-    public static native String basename(String path);
-    public static native String dirname(String path);
-    public static class Extras {
-        public static native String resolvePath(String cwd, String path);
-        public static native String resolveDirname(String cwd, String path);
+
+    private static JNI_CPP_API instance;
+
+    static {
+        System.loadLibrary("AndroidDAW_JniHelpers");
+        System.loadLibrary("AndroidDAW_JNI_CPP_API");
+    }
+
+    public final Extras Extras;
+
+    private JNI_CPP_API() {
+        Extras = new Extras();
+    }
+
+    public static JNI_CPP_API getInstance() {
+        if (instance == null) {
+            instance = new JNI_CPP_API();
+        }
+        return instance;
+    }
+
+    public native String basename(String path);
+
+    public native String dirname(String path);
+
+    public class Extras {
+        public native String resolvePath(String cwd, String path);
+
+        public native String resolveDirname(String cwd, String path);
     }
 }
